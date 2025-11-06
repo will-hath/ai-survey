7cbb753f801cd760f2c77c0e1aa745d0174ccc01
will-hath <willhath@mit.edu>
2025-11-05 19:52:10 -0500

    its the import?

--- api/index.py
+++ api/index.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import os
 from functools import wraps
 
-from openai.types.responses.response_create_params import ResponseCreateParamsNonStreaming
+# from openai.types.responses.response_create_params import ResponseCreateParamsNonStreaming
 
 # PROMPT_ID = "pmpt_690ba19536b88197a1c48fb6c5d8d6380b17d1daec64b5f4"
 
