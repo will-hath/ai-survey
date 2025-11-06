43c7c8e9ecda311174d4331dcba24d05d6109a0f
will-hath <willhath@mit.edu>
2025-11-05 19:49:58 -0500

    import only

--- api/index.py
+++ api/index.py
@@ -9,17 +9,17 @@ from functools import wraps
 
 from openai.types.responses.response_create_params import ResponseCreateParamsNonStreaming
 
-PROMPT_ID = "pmpt_690ba19536b88197a1c48fb6c5d8d6380b17d1daec64b5f4"
+# PROMPT_ID = "pmpt_690ba19536b88197a1c48fb6c5d8d6380b17d1daec64b5f4"
 
-def make_response_request(input: str, conversation_id: str) -> ResponseCreateParamsNonStreaming:
-    return ResponseCreateParamsNonStreaming(
-        model="gpt-5-nano",
-        prompt={
-            "id": PROMPT_ID,
-        },
-        input=input,
-        conversation=conversation_id
-    )
+# def make_response_request(input: str, conversation_id: str) -> ResponseCreateParamsNonStreaming:
+#     return ResponseCreateParamsNonStreaming(
+#         model="gpt-5-nano",
+#         prompt={
+#             "id": PROMPT_ID,
+#         },
+#         input=input,
+#         conversation=conversation_id
+#     )
 
 
 # logger = logging.getLogger(__name__)
